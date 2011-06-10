6486e6005d13739a05965985e41f89391f75fbec
Grady Laksmono <gradyfausta@laksmono.com>
2011-06-10 00:43:46 -0700

    * Login using Facebook

---
+++ js/fb.js
@@ -0,0 +1,35 @@
+/*
+ * Author : Grady Laksmono
+ * Email : grady@eventfii.com
+ * All code (c) 2011 Eventfii Inc. 
+ * All rights reserved
+ */
+
+var FBCON = (function() {
+	return {
+		init: function() {
+			FB.init({
+				appId  : '114335368653053',
+				status : true, // check login status
+				cookie : true, // enable cookies to allow the server to access the session
+				xfbml  : true  // parse XFBML
+			});
+			
+			FB.getLoginStatus(function(response) {
+				if (response.session) {
+					FBCON.onlogin();
+				} else {
+					
+				}
+			});
+		},
+		
+		onlogin: function() {
+			FB.api('/me', function(userInfo) {
+				LOGIN_FORM.fbUserLogin(userInfo);
+			});
+		}
+	}
+})();
+
+FBCON.init();
