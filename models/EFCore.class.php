1b5e719b28f5b5e026c0c5c9d31a51e948948ffc
Grady Laksmono <gradyfausta@laksmono.com>
2011-06-24 00:28:24 -0700

    * Implemented the Guestimate algorithm [2.0]   Guestimate = G(Not responded) + F(Responded)   Where G = Guestimate,         F = TrueRSVP w/o guests who haven't responded

--- models/EFCore.class.php
+++ models/EFCore.class.php
@@ -22,8 +22,8 @@ class EFCore {
 	}
 	
 	public function computeGuestimate($eid) {
-		$numGuests = $this->dbCon->getNumGuests($eid);
-		$numGuests = $numGuests['num_guests'];
+		$numGuests = $this->dbCon->getNumAttendeesByConfidence($eid, CONFELSE);;
+		$numGuests = $numGuests['guest_num'];
 		
 		$baseNum = 0;
 		if ($numGuests >= 0 && $numGuests <= 20) {
@@ -34,9 +34,31 @@ class EFCore {
 			$baseNum = mt_rand(GUESTRANGE3MIN, GUESTRANGE3MAX);
 		}
 		
-		$guestimate = $numGuests * $this->efCom->toPercent($baseNum);
+		$guestimate = ($numGuests * $this->efCom->toPercent($baseNum)) + $this->computeGuestimateTRSVP($eid);
 		
 		return round($guestimate, 0);
+	}
+	
+	// Guestimate TrueRSVP does not include the probability of the guests
+	// Those are not responding to the invitation
+	public function computeGuestimateTRSVP($eid) {
+		$numGuestConf1 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT1);
+		$numGuestConf2 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT2);
+		$numGuestConf3 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT3);
+		$numGuestConf4 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT4);
+		$numGuestConf5 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT5);
+		$numGuestConf6 = $this->dbCon->getNumAttendeesByConfidence($eid, CONFOPT6);
+		
+		$trsvpConf1Val = $this->efCom->toPercent(CONFOPT1) * $numGuestConf1['guest_num'];
+		$trsvpConf2Val = $this->efCom->toPercent(CONFOPT2) * $numGuestConf2['guest_num'];
+		$trsvpConf3Val = $this->efCom->toPercent(CONFOPT3) * $numGuestConf3['guest_num'];
+		$trsvpConf4Val = $this->efCom->toPercent(CONFOPT4) * $numGuestConf4['guest_num'];
+		$trsvpConf5Val = $this->efCom->toPercent(CONFOPT5) * $numGuestConf5['guest_num'];
+		$trsvpConf6Val = $this->efCom->toPercent(CONFOPT6) * $numGuestConf6['guest_num'];
+		
+		$trsvpVal = $trsvpConf1Val + $trsvpConf2Val + $trsvpConf3Val + 
+								$trsvpConf4Val + $trsvpConf5Val + $trsvpConf6Val;
+		return round($trsvpVal, 0);
 	}
 	
 	public function computeTrueRSVP($eid) {
