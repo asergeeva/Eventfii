da257ebc97c69c44dec6d6bde37118ae90e4c9d8
Grady Laksmono <gradyfausta@laksmono.com>
2011-07-31 23:16:08 -0700

    * Implemented Gmail and Yahoo contact importer

--- libs/OpenInviter/config.php
+++ libs/OpenInviter/config.php
@@ -3,8 +3,8 @@
 		"username"=>"glaksmono",
 		"private_key"=>"a274603b9204e5bf1142a06c8349537b",
 		"cookie_path"=>'/tmp',
-		"message_body"=>"You are invited to www.laksmono.com", // www.laksmono.com is the website on your account. If wrong, please update your account at OpenInviter.com
-		"message_subject"=>" is inviting you to www.laksmono.com", // www.laksmono.com is the website on your account. If wrong, please update your account at OpenInviter.com
+		"message_body"=>"You are invited to www.truersvp.com", // www.laksmono.com is the website on your account. If wrong, please update your account at OpenInviter.com
+		"message_subject"=>" is inviting you to www.truersvp.com", // www.laksmono.com is the website on your account. If wrong, please update your account at OpenInviter.com
 		"transport"=>"curl", //Replace "curl" with "wget" if you would like to use wget instead
 		"local_debug"=>"on_error", //Available options: on_error => log only requests containing errors; always => log all requests; false => don`t log anything
 		"remote_debug"=>FALSE, //When set to TRUE OpenInviter sends debug information to our servers. Set it to FALSE to disable this feature
