52ba831c3151c7ee4bee184b07ce132e06b58873
Nicholas Chan <movingincircles@gmail.com>
2011-08-04 03:17:20 -0700

    APIController/DBAPI methods added

--- controller/APIController.class.php
+++ controller/APIController.class.php
@@ -25,6 +25,27 @@ class APIController {
 		
 	}
 	
+	private function buildEvent($eventId) {
+		$eventInfo = $this->dbCon->getEventInfo($eventId);
+		
+		$eventInfo['address'] = $eventInfo['location_address'];
+		
+		$eventDateTime = explode(" ", $eventInfo['event_datetime']);
+		
+		$eventInfo['date'] = $this->dbCon->dateToRegular($eventDateTime[0]);		
+        $eventInfo['deadline'] = $this->dbCon->dateToRegular($eventInfo['event_deadline']);
+		
+		$eventTime = explode(":", $eventDateTime[1]);
+		$eventInfo['time'] = $eventTime[0].":".$eventTime[1];
+		
+		$this->smarty->assign('eventInfo', $eventInfo);
+		
+		require_once(realpath(dirname(__FILE__)).'/../models/Event.class.php');
+		$event = new Event( $eventInfo );
+		
+		return $event;
+	}
+	
 	public function getView($requestUri) {
 		$requestUri = str_replace(PATH, '', $requestUri);
 		$requestUri = explode('/', $requestUri);
@@ -36,7 +57,7 @@ class APIController {
 				{
 					if(isset($_REQUEST['email']) && isset($_REQUEST['password']))
 					{
-						$result = $this->dbCon->checkValidUserMobile($_REQUEST['email'], $_REQUEST['password']);
+						$result = $this->dbCon->m_checkValidUser($_REQUEST['email'], $_REQUEST['password']);
 						if(!isset($result))
 						{
 							echo 'status_loginFailed';
@@ -56,13 +77,13 @@ class APIController {
 				echo json_encode($this->dbCon->getUserInfo($_SESSION['uid']));
 				break;
 			case 'setUserInfo':
-				echo $this->dbCon->updateUserInfoMobile($_REQUEST['email'],$_REQUEST['about'],$_REQUEST['zip'],$_REQUEST['cell'],$_REQUEST['twitter']);
+				echo $this->dbCon->m_updateUserInfo($_REQUEST['email'],$_REQUEST['about'],$_REQUEST['zip'],$_REQUEST['cell'],$_REQUEST['twitter']);
 				break;
 			case 'getOrganizerEmail':
 				echo json_encode($this->dbCon->getUserInfo($_REQUEST['oid']));
 				break;
 			case 'getAttendingEvents':
-				echo json_encode($this->dbCon->getEventAttendingBy($_SESSION['uid']));
+				echo json_encode($this->dbCon->m_getEventAttendingBy($_SESSION['uid']));
 				break;
 			case 'setAttendanceForEvent':
 				echo json_encode($this->dbCon->eventSignUp($_SESSION['uid'], $_REQUEST['eid'], $_REQUEST['confidence']));
@@ -71,7 +92,7 @@ class APIController {
 				echo json_encode($this->dbCon->getEventByEO($_SESSION['uid']));				
 				break;
 			case 'getGuestList':
-				echo json_encode($this->dbCon->getAttendeesByEvent($_REQUEST['eid']));
+				echo json_encode($this->dbCon->m_getGuestListByEvent($_REQUEST['eid']));
 				break; 
 			case 'checkInByDistance':
 				$this->dbCon->checkInGuest('1', $_SESSION['uid'], $_REQUEST['eid']);
@@ -82,6 +103,23 @@ class APIController {
 				break;
 			case 'computeTrueRSVP':
 				echo json_encode($this->efCore->computeTrueRSVP($_REQUEST['eid']));
+				break;
+			case 'sendMessage':
+				require_once(realpath(dirname(__FILE__)).'/../models/EFMail.class.php');
+				$event = $this->buildEvent( $_REQUEST['eventId'] );		
+				$mailer = new EFMail();
+				$contacts = $_REQUEST['uid'];
+				//Newbie php coding skills:
+				$attendees = array();
+				for($i=0; $i < count($contacts); $i++)
+				{
+					array_push($attendees, $this->dbCon->getUserInfo($contacts[$i]));
+				}
+				$mailer->sendAutomatedEmail($event, 
+											$_REQUEST['reminderContent'], 
+											$_REQUEST['reminderSubject'], 
+											$attendees);
+				echo("status_emailSuccess");
 				break;
 			default:
 				$this->smarty->assign('requestUri', $requestUri);
