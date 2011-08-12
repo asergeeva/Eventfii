381646d7da5617e3bf18c74ba37c6ca0ef5ecbc7
Grady Laksmono <gradyfausta@laksmono.com>
2011-08-12 01:14:20 -0700

    Fixing the APIController from the result of refactoring

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-require_once(realpath(dirname(__FILE__)).'../configs.php'));
+require_once(realpath(dirname(__FILE__)).'/../configs.php');
 require_once(realpath(dirname(__FILE__)).'/../models/EFCommon.class.php');
-require_once(realpath(dirname(__FILE__)).'/../controller/APIController.class.php'));
+require_once(realpath(dirname(__FILE__)).'/../controller/APIController.class.php');
 
 $common = new EFCommon($smarty);
 $panelController = new APIController();
