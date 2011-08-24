98d3a98c9c9b214b1633cdfb78c2e40f30f5c4f1
Grady Laksmono <gradyfausta@laksmono.com>
2011-08-24 03:09:01 -0700

    Refactored the configuration files

---
+++ baseconfig.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', '127.0.0.1:3306');
+define('DB_USER', 'glaksmono');
+define('DB_PASS', '12345');
+define('DB_NAME', 'eventfii');
+define('DEBUG', true);
+
+define('PATH', '/Eventfii');
+define('CURHOST', 'http://localhost'.PATH);
