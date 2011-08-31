461f0463336b2cabb0ec06a43735bb2ac0ecc2a3
Grady Laksmono <gradyfausta@laksmono.com>
2011-08-31 09:06:12 -0700

    Make sure that friends is being stored before redirecting

--- js/login.js
+++ js/login.js
@@ -52,14 +52,14 @@ var LOGIN_FORM = (function() {
 				curPage: window.location.href,
 				fb_access_token: sessionInfo.access_token,
 				fb_session_key: sessionInfo.session_key
-			}, LOGIN_FORM.loginRedirect);
-			
-			FB.api('/me/friends?access_token=' + sessionInfo.access_token, function(userFriends) {
-				if (typeof userFriends.error == 'undefined') {
-					$.post(EFGLOBAL.baseUrl + '/fb/friends', {
-						fbFriends: JSON.stringify(userFriends)
-					});
-				}
+			}, function() {
+				FB.api('/me/friends?access_token=' + sessionInfo.access_token, function(userFriends) {
+					if (typeof userFriends.error == 'undefined') {
+						$.post(EFGLOBAL.baseUrl + '/fb/friends', {
+							fbFriends: JSON.stringify(userFriends)
+						}, LOGIN_FORM.loginRedirect);
+					}
+				});
 			});
 		},
 		
