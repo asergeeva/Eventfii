cd5361d87b20e4f90dd60f237295924e976293bb
Adam Wexler <adamrwexler@gmail.com>
2011-09-11 17:15:26 -0700

    Default thumb for user

--- controller/PanelController.class.php
+++ controller/PanelController.class.php
@@ -735,7 +735,6 @@ class PanelController {
 		switch ($current_page) {
 			case '/':
 			case '/home':
-				print_r($_SESSION['user']);
 				if (isset($_SESSION['isNewUser'])) {
 					EFCommon::$smarty->assign('isNewUser', true);
 					unset($_SESSION['isNewUser']);
