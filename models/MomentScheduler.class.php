8925629fa700829d56fdbac7bf8dc5800e465a17
Grady Laksmono <gradyfausta@laksmono.com>
2011-10-24 21:36:40 -0700

    Fixed the cron job

---
+++ models/MomentScheduler.class.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Author : Grady Laksmono
+ * Email : grady@truersvp.com
+ * All code (c) 2011 trueRSVP Inc. 
+ * All rights reserved
+ */
+class MomentScheduler {
+	public function __construct() {
+	
+	}
+	
+	public function __destruct() {
+	
+	}
+	
+	
+}
