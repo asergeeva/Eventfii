127ec867899f46b48f5dba1b1cb8f787536b7c9d
Grady Laksmono <gradyfausta@laksmono.com>
2011-10-31 12:08:11 -0700

    Fixed pictures on the RSVP breakdown

--- baseconfig.php
+++ baseconfig.php
@@ -6,24 +6,24 @@
  * All rights reserved
  */
 
-define('DB_HOST', 'internal-db.s99541.gridserver.com:3306');
-define('DB_USER', 'db99541_true_qa');
-define('DB_PASS', 'happyparty');
-define('DB_NAME', 'db99541_true_qa');
-define('DEBUG', false);
+define('DB_HOST', '127.0.0.1:3306');
+define('DB_USER', 'glaksmono');
+define('DB_PASS', '12345');
+define('DB_NAME', 'eventfii');
+define('DEBUG', true);
 define('ENABLE_EMAIL', true);
 
-define('PATH', '');
-define('CURHOST', 'http://qa.truersvp.com'.PATH);
+define('PATH', '/Eventfii');
+define('CURHOST', 'https://localhost'.PATH);
 
-define('FB_BASE_URL', 'http://apps.facebook.com/truersvp_qa');
-define('FB_GRAPH_URL', 'http://graph.facebook.com');
-define('FB_APP_ID', '256152217746559');
-define('FB_APP_SECRET', 'eda14af05705ec62098266b5d2bc81d1');
+define('FB_BASE_URL', 'https://apps.facebook.com/truersvp_localhost');
+define('FB_GRAPH_URL', 'https://graph.facebook.com');
+define('FB_APP_ID', '122732554481304');
+define('FB_APP_SECRET', '2416d6b193a5c135c2c850b02a4dba78');
 
-define('GOOGLE_MAPS_API', 'ABQIAAAAYw9IaNOSbAwS1vK0gtJ__xSg30_YD3IIP8NVVjYl9YkEYp2gBRQqLTqNrejOlyChi__bADM7NNaD9A');
+define('GOOGLE_MAPS_API', 'ABQIAAAAYw9IaNOSbAwS1vK0gtJ__xT2yXp_ZAY8_ufC3CFXhHIE1NvwkxRy3n3lZ15FCAqVglN8_EtnpZDXbg');
 define('GOOGLE_MAP_URL', 'https://maps.google.com/maps');
 
-define('TWITTER_API', 'ABbFBaUGZRWW5NGwVkU1oQ');
+define('TWITTER_API', 'NdOFEfRSe5BNdveGsWBIpg');
 
-define('ALL_REPORTING', 0);
+define('ALL_REPORTING', E_ALL);
